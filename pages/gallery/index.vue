3e1e14800282517739cb5cb85b8daa96e291386c
Miikka <miikka.makela@into-digital.fi>
2022-02-03 21:09:49 +0200

    Gallery mess.

--- pages/gallery/index.vue
+++ pages/gallery/index.vue
@@ -3,43 +3,23 @@
         <h1 id="page_insight">Design is not only about putting stuff in—but it is also about leaving stuff out.</h1>
         <div class="images_cont">
             <div>
-                <gallery :images="images" :index="index" @close="index = null"></gallery>
-                <div
-                    class="image"
-                    v-for="(image, imageIndex) in images"
-                    :key="imageIndex"
-                    @click="index = imageIndex"
-                    :style="{ backgroundImage: 'url(' + image + ')', width: '300px', height: '200px' }"
-                ></div>
+
             </div>
         </div>
     </div>
 </template>
 
 <script>
-import VueGallery from 'vue-gallery'
+
 
 export default {
 
     data() {
         return {
-            images: [
-                'https://dummyimage.com/800/ffffff/000000',
-                'https://dummyimage.com/1600/ffffff/000000',
-                'https://dummyimage.com/1280/000000/ffffff',
-                'https://dummyimage.com/400/000000/ffffff',
-            ],
-            index: null
+
         }
     },
 
-    components: {
-      'gallery': VueGallery
-    },
-
-    mounted() {
-     
-    },
     head () {
         return {
             title: 'Gallery ∙ MM',
@@ -47,12 +27,13 @@ export default {
                 { 
                     hid: 'Miikka Mäkelä gallery',
                     name: 'Gallery page for designs',
-                    content: 'Showcasing my design expertise.'
+                    content: 'Showcasing my designs.'
                 }
             ]
         }
     }
 }
+
 </script>
 
 <style lang="scss" scoped>
